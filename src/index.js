89fdc0a0b6b082c02b0bc214a254b7f6a77fcf5c
umayanga <umayangag@gmail.com>
2022-04-07 14:13:19 +0530

    remove base 2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 
-const routerBaseName = process.env.REACT_APP_PUBLIC_URL;
-
 ReactDOM.render(
     <BrowserRouter>
         <App/>
