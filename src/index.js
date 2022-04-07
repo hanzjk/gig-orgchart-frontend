a38652d48af9ba9996242213fe216111f55526f1
umayanga <umayangag@gmail.com>
2022-04-07 10:44:54 +0530

    Revert "add base name to router"
    This reverts commit 6cf0953fb6026b998ecdfb20b999caa5c0bcef10.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 
-const routerBaseName = process.env.PUBLIC_URL;
-
 ReactDOM.render(
-    <BrowserRouter basename={routerBaseName}>
+    <BrowserRouter>
         <App/>
     </BrowserRouter>,
     document.getElementById('root')
